1da78dd4832614e8a7674af41cc220921ac1c06b
Jovicy <johnvictordml@gmail.com>
2026-05-24 01:15:22 +0100

    edited the mail on the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="text-center mt-5 text-[12px] text-slate-400">
-      © 2026 Forbes Royal College. All rights reserved.  |  admissions@forbesroyalcollege.ac.uk
+      © 2026 Forbes Royal College. All rights reserved.  |  info@forbesroyalschools.com
     </div>
   )
 }
